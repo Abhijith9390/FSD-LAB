0de1399913851e0efed35cf40ca5410f3388b1c0
Abhijith9390 <bachuabhijith65@gmail.com>
2026-07-29 16:23:59 +0530

    Add files via upload

---
+++ lab1.js
@@ -0,0 +1,5 @@
+import chalk from "chalk";
+console.log(chalk.red("Name: Abhijith"));
+console.log(chalk.green("Roll.No: B24CS272"));
+console.log(chalk.yellow("Branch: CSE"));
+console.log(chalk.purle("Section: 5"));
